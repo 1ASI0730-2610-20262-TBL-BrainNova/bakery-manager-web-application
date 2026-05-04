0ae3810c29c72ab3ae1d03ef201e9d69d4f55812
Jarod Cespedes <u202318588@upc.edu.pe>
2026-05-04 09:51:14 -0500

    chore: initial commit (main project files, unstable).

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './shared/presentation/views/home/home';
 
-export const routes: Routes = [];
+const about = () => import('./shared/presentation/views/about/about').then((m) => m.About);
+const pageNotFound = () =>
+  import('./shared/presentation/views/page-not-found/page-not-found').then((m) => m.PageNotFound);
+
+const baseTitle = 'BakeryManager';
+
+export const routes: Routes = [
+  { path: 'home', component: Home, title: `${baseTitle} - Home` },
+  { path: 'about', loadComponent: about, title: `${baseTitle} - About` },
+  // TODO: Add more paths here!
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', loadComponent: pageNotFound, title: `${baseTitle} - Page Not Found` },
+];
