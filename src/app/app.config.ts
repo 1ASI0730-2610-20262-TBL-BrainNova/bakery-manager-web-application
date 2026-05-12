ba0b7154698036515daa042e431c1434987f3378
Jarod Cespedes <u202318588@upc.edu.pe>
2026-05-12 18:46:33 -0500

    chore: add ng2-charts dependencies, and configuration.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideTranslateService } from '@ngx-translate/core';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,7 +14,8 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideTranslateService({
       loader: provideTranslateHttpLoader({ prefix: './i18n/', suffix: '.json' }),
-      fallbackLang: 'en'
-    })
-  ]
+      fallbackLang: 'en',
+    }),
+    provideCharts(withDefaultRegisterables()),
+  ],
 };
