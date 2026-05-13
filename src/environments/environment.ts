dfdd24d0d1da4a02c44ec94ecc012fdeefb094f2
Jarod Cespedes <u202318588@upc.edu.pe>
2026-05-13 08:21:23 -0500

    feat(env): updated environment variables.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,4 +5,13 @@ export const environment = {
   monitoringProviderSensorReadingsEndpointPath: '/readings',
   monitoringProviderIncidentsEndpointPath: '/incidents',
   monitoringProviderAlertsEndpointPath: '/alerts',
+  firebase: {
+    apiKey: 'AIzaSyB13aCI_-AWcwuSa9YPYoE6UQ_5CjOLiEc',
+    authDomain: 'bakerymanager.firebaseapp.com',
+    projectId: 'bakerymanager',
+    storageBucket: 'bakerymanager.firebasestorage.app',
+    messagingSenderId: '362412046305',
+    appId: '1:362412046305:web:6aadf14e30a688bf1d44a0',
+    measurementId: 'G-5LZXDVTP0E',
+  },
 };
