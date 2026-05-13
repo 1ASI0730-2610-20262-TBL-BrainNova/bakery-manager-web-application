180ee0fb8574ac26436ed6ddee8197c6ccc137d9
Jarod Cespedes <u202318588@upc.edu.pe>
2026-05-12 22:00:22 -0500

    feat(presentation): add sensor grid component to the dashboard.

--- src/app/monitoring/presentation/views/dashboard/dashboard.ts
+++ src/app/monitoring/presentation/views/dashboard/dashboard.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TopSummary } from '../../components/top-summary/top-summary';
+import { SensorGrid } from '../../components/sensor-grid/sensor-grid';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [TopSummary],
+  imports: [TopSummary, SensorGrid],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
